6e5f00a31ea737d2d181a6cb854f2a0462b0b09f
Lucas <starlucas68@laposte.net>
2021-01-02 14:15:05 +0100

    Added flash module

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+
 const bodyParser = require("body-parser");
+const flash = require("connect-flash");
+
 const animaux = require("./routes/animaux");
 
 app.use(bodyParser.json());
+//app.use(flash());
 app.use(express.static(__dirname + "/public"));
 app.set("views", __dirname + "/views");
 
