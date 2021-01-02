7a2d3af444f49fb40fd563e62db79c9e46a88ed0
Lucas <starlucas68@laposte.net>
2021-01-02 21:14:31 +0100

    Added edit animal

--- routes/animaux.js
+++ routes/animaux.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Twig = require("twig");
 const db = require("../db");
 const { body, validationResult } = require('express-validator');
+const csurf = require('csurf');
 
 router.get("/", (req, res) => {
 	db.query('SELECT animal.*, type_animal.libelle as type_libelle FROM animal JOIN type_animal on type_animal.id = animal.type_animal_id', (err, listeAnimaux) => {
@@ -26,7 +27,7 @@ router.get("/add", (req, res) => {
 router.post(
 	"/add",
 	body('type_animal_id').trim().not().equals('0').withMessage("Veuillez choisir un type d'animal"),
-	body('nom_animal').trim().isLength({ min: 5 }).withMessage('Le nom doit faire au moins 5 caractères').isAlpha().withMessage('Le nom doit contenir uniquement des lettres'),
+	body('nom_animal').trim().isLength({ min: 3 }).withMessage('Le nom doit faire au moins 5 caractères').isAlpha().withMessage('Le nom doit contenir uniquement des lettres'),
 	body('prix_achat').trim().isLength({ min: 1 }).withMessage("Le prix ne peut pas être vide").isNumeric().withMessage("Le prix ne doit contenir que des chiffres"),
 	//body('date_naissance').trim().matches("/^\d{2}[/]\d{2}[/]\d{4}$/").withMessage("La date de naissance doit être sous la forme dd/mm/aaaa"),
 	body('couleur').trim().isLength({ min: 1 }).withMessage("La couleur ne peut pas être vide"),
@@ -92,5 +93,54 @@ router.post("/:id/delete", (req, res) => {
 		else res.status(500).send(err);
 	});
 });
+
+router.get("/:id/edit", (req, res) => {
+	const id = req.params.id;
+
+	db.query('SELECT * FROM type_animal', (err, listeTypesAnimaux) => {
+		if(!err) db.query('SELECT * FROM animal WHERE id = ' + id, (err, animal) => {
+			if(!err) {
+				if (animal[0] === undefined) res.status(404).end();
+				else {
+					const date = new Date(animal[0].date_naissance);
+					console.log(date);
+					animal[0].date_naissance = date.getDate().toString().padStart(2, "0") + "/" + date.getMonth().toString().padStart(2, "0") + "/" + date.getFullYear();
+					res.render("animal_edit.twig", { animal: animal[0], typesAnimal: listeTypesAnimaux });
+				}
+			}
+			else res.status(500).send(err);
+		});
+		else res.status(500).send(err);
+	});
+});
+
+router.post(
+	"/:id/edit",
+	body('type_animal_id').trim().not().equals('0').withMessage("Veuillez choisir un type d'animal"),
+	body('nom_animal').trim().isLength({ min: 3 }).withMessage('Le nom doit faire au moins 5 caractères').isAlpha().withMessage('Le nom doit contenir uniquement des lettres'),
+	body('prix_achat').trim().isLength({ min: 1 }).withMessage("Le prix ne peut pas être vide").isNumeric().withMessage("Le prix ne doit contenir que des chiffres"),
+	//body('date_naissance').trim().matches("/^\d{2}[/]\d{2}[/]\d{4}$/").withMessage("La date de naissance doit être sous la forme dd/mm/aaaa"),
+	body('couleur').trim().isLength({ min: 1 }).withMessage("La couleur ne peut pas être vide"),
+	body('poids').trim().isLength({ min: 1 }).withMessage("Le poids ne peut pas être vide").isNumeric().withMessage("Le poids ne doit contenir que des chiffres"),
+	body('taille').trim().isLength({ min: 1 }).withMessage("La taille ne peut pas être vide").isNumeric().withMessage("La taille ne doit contenir que des chiffres"),
+	(req, res) => {
+		const id = req.params.id;
+		const data = req.body;
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			db.query('SELECT * FROM type_animal', (err, listeTypesAnimaux) => {
+				if(!err) res.render("animal_edit.twig", { animal: data, typesAnimal: listeTypesAnimaux, errors: errors.errors });
+				else res.status(500).send(err);
+			});
+		}
+		else {
+			const date = data.date_naissance.split("/")[2] + "-" + data.date_naissance.split("/")[1] + "-" + data.date_naissance.split("/")[0];
+			db.query("UPDATE animal SET type_animal_id=" + data.type_animal_id + ", nom_animal='" + data.nom_animal + "', prix_achat=" + data.prix_achat + ", date_naissance='" + date + "', couleur='" + data.couleur + "', poids=" + data.poids + ", taille=" + data.taille + " WHERE id=" + id, (err) => {
+				if(!err) res.redirect("/animaux");
+				else res.status(500).send(err);
+			});
+		}
+	}
+);
 
 module.exports = router;
